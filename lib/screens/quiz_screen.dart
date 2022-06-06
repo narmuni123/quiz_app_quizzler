42ecff8118523aaf4c018aade1c8ad7f3d309732
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-06 16:53:39 +0530

    added list of widgets

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -8,6 +8,9 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+
+  List<Widget> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,67 +20,64 @@ class _QuizScreenState extends State<QuizScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            const Expanded(
-              flex: 5,
-              child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text(
-                    'This is where the question text will go.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
+            questionTile(question: "This is where the question text will go."),
+            buttonWidget(buttonColor: Colors.green, buttonText: "True", callback: (){
+              setState(() {
+                scoreKeeper.add(const Icon(Icons.check, color: Colors.green,));
+              });
+            }),
+            buttonWidget(buttonColor: Colors.red, buttonText: "False", callback: (){
+              setState(() {
+                scoreKeeper.add(const Icon(Icons.close, color: Colors.red,));
+              });
+            }),
+
+            Row(
+              children: scoreKeeper,
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                      Colors.green,
-                    ),),
-                  child: const Text(
-                    'True',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  onPressed: () {
-                    //The user picked true.
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                      Colors.red,
-                    ),
-                  ),
-                  child: const Text(
-                    'False',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {
-                    //The user picked false.
-                  },
-                ),
-              ),
-            ),
-            //TODO: Add a Row here as your score keeper
+
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget questionTile({required String question}){
+    return Expanded(
+      flex: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Text(
+            question,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 25.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buttonWidget({required Color buttonColor, required String buttonText, required VoidCallback callback}){
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              buttonColor,
+            ),),
+          child:  Text(
+            buttonText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+          onPressed: callback,
         ),
       ),
     );
